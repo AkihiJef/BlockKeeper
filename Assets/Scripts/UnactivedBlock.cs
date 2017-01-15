559039f2a4da7be6de6a5f4ca26cb17822d3fb22
穆安 <AkihiJef@192.168.0.103>
2017-01-15 11:08:51 +0800

    first commit

---
+++ Assets/Scripts/UnactivedBlock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnactivedBlock : MonoBehaviour {
+    public GameObject ball;
+    public bool init=false;
+    public GameObject blockPrefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (init && ball.transform.position.x > 0)
+        {
+            GameObject block = Instantiate(blockPrefab, transform.position,transform.rotation) as GameObject;
+            block.GetComponent<SpriteRenderer>().sortingLayerName = "Object";
+            block.GetComponent<SpriteRenderer>().sortingOrder = -1;
+            Destroy(gameObject);
+        }
+	}
+}
